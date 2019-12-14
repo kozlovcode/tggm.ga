9a08398e4849bfe44beefcb0ceb4a52e21875c57
kozlovcode <romandkoz@gmail.com>
2019-12-14 14:38:20 +0300

    Add redirect to proxy

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 $request_url = explode('/', $_SERVER['REQUEST_URI']);
 $domain = $request_url[1];
 $chat = $request_url[2];
+$proxy = explode('?', $domain);
 header("Location: tg://");
 if ($domain) {
 	if ($chat) {
 		header("Location: tg://join?invite=$chat");
+	}elseif($proxy[0] == 'proxy'){
+		header("Location: tg://$domain");
 	}else{
 		header("Location: tg://resolve?domain=$domain");
 	}	
